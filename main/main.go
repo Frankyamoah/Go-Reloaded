ba05bb470ad83030489a309387f6dc944dd33276
fyamoah <frankyamoah94@gmail.com>
2022-10-10 13:19:23 +0100

    second commit

--- main/main.go
+++ main/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	// This reads the contents of the file
+	contents, err := os.ReadFile(os.Args[1])
+	// Checks for errors
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Converts arguements into string
+	stringContent := string(contents)
+
+	// Converts string into string Array
+	wordArray := strings.Fields(stringContent)
+
+	// Array of keywords to search through
+	keyWordArray := [8]string{"(hex)", "(bin)", "(up)", "(up,", "(low)", "(low,", "(cap),", "(cap,"}
+}
