bc4653542342a7ade1540b2c37b03c0cd2c0da13
fyamoah <frankyamoah94@gmail.com>
2022-10-10 16:40:22 +0100

    djdj

--- main/main.go
+++ main/main.go
@@ -22,10 +22,19 @@ func main() {
 	// Array of keywords to search through
 	keyWordArray := [8]string{"(hex)", "(bin)", "(up)", "(up,", "(low)", "(low,", "(cap),", "(cap,"}
 
+	punctuationArray := [6]string{".", ",", "!", "?", ":", ";"}
+
 	// loop through array of arguements
 	for i := 0; i < len(wordArray); i++ {
 		// loop through array of keyWord
 		for j := 0; j < len(keyWordArray); j++ {
+			// loop through punctuation Array
+			for k := 0; k < len(punctuationArray); k++ {
+				for k := 0; k < len(punctuationArray); k++ {
+					if (wordArray[i] == keyWordArray[0]) && (keyWordArray[j] == keyWordArray[0]) && (wordArray[i] == keyWordArray[j]) {
+					}
+				}
+			})
 		}
 	}
 }
